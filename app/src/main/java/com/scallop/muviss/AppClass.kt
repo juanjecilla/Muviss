3b4569669bde231996996ffde358b3d9bf3d8432
JuanjeCilla <juanje.cilla@gmail.com>
2021-11-19 11:22:05 +0100

    Strict mode active

--- app/src/main/java/com/scallop/muviss/AppClass.kt
+++ app/src/main/java/com/scallop/muviss/AppClass.kt
@@ -1,7 +1,32 @@
 package com.scallop.muviss
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AppClass : Application()
+class AppClass : Application() {
+    override fun onCreate() {
+        if (DEVELOPER_MODE) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyFlashScreen()
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
+        super.onCreate()
+    }
+
+    companion object {
+        const val DEVELOPER_MODE = true
+    }
+}
